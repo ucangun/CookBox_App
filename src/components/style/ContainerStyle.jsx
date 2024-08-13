f28eb839ca11e0728a9ebbee4f49af2fffdc1630
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-13 14:15:31 +0200

    "Added global CSS styles and wrapped Home component with Container component"

---
+++ src/components/style/ContainerStyle.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: orangered;
+  height: 97vh;
+  margin: 1rem;
+  padding: 1rem;
+  border-radius: 1rem;
+`;
+
+export default Container;
