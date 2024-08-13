7618a6cb63a873043686c925f0ca9ed4fb7fa104
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-13 22:28:09 +0200

    "Changed 'height' to 'min-height' in ContainerStyle.jsx"

--- src/components/style/ContainerStyle.jsx
+++ src/components/style/ContainerStyle.jsx
@@ -4,7 +4,7 @@ import HomepageImg from "../../assets/images/Homepage.jpg";
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
+  min-height: 100vh;
   padding: 0.4rem 2rem 0 1.5rem;
   background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.5)),
     url(${HomepageImg}) no-repeat center center/cover;
